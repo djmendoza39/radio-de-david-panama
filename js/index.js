8a33d754e23909c068a366d2bb098881f191c2a0
Darwin Junior <djmendoza39@gmail.com>
2022-06-27 18:30:55 -0500

    actualizacion en varias secciones de la web

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+const iconoMenu = document.querySelector('#iconoMenu');
+    menu = document.querySelector('#menu');
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
-
-  if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
+iconoMenu.addEventListener('click', (e)=>{
+    //donde llamamos los eventos
+    menu.classList.toggle('active');
+    
 });
 
